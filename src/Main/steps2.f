7126d2bd8669ddfcf3d5ce3b53879803b6856198
Long Wang <longwang.astro@gmail.com>
2015-05-26 23:33:47 +0800

    Suppress fpoly2 in initialization and use V,F,Fdot for step criterion for first time step

---
+++ src/Main/steps2.f
@@ -0,0 +1,64 @@
+      SUBROUTINE STEPS2(I1,I2)
+*
+*
+*       Initialization of time-steps by Xdot, F and Fdot & prediction variables.
+*       ----------------------------------------------------
+*
+      INCLUDE 'common6.h'
+      REAL*8  FDUM(3)
+*
+*
+*       Set new steps and initialize prediction variables.
+!$omp parallel do private(I,K,DT,DTR,DTN,DTRN,FDUM)
+      DO 40 I = I1,I2
+*
+*       Include regular force in the irregular step (cf. Makino & SJA 1992).
+          DO 5 K = 1,3
+              FDUM(K) = FI(K,I) + FR(K,I)
+    5     CONTINUE
+*
+*       Determine irregular and regular steps by the general criterion.
+          DT = TSTEP2(XDOT(1,I),FDUM,D1(1,I),ETAI)
+          DTR = TSTEP2(XDOT(1,I),FR(1,I),D1R(1,I),ETAR)
+*
+*       Initialize the times and obtain discrete steps (block-step version).
+          T0(I) = TIME
+          T0R(I) = TIME
+*
+*     Convert predicted step to nearest block time-step (truncated down).
+          CALL STEPK(DT,DTN)
+          CALL STEPK(DTR,DTRN)
+*     --11/22/13 20:26-lwang-debug--------------------------------------*
+***** Note:------------------------------------------------------------**
+c$$$         IF(I.EQ.12534.and.rank.eq.0) then
+c$$$             print*,'I',I,'T',time,'TB',tblock,'DT',dt,'DTN',dtn
+c$$$          end if
+*     --11/22/13 20:26-lwang-end----------------------------------------*
+          STEP(I) = DTN
+          STEPR(I) = DTRN
+
+*       Reduce regular step if STEPR > SMAX
+          STEPR(I) = MIN(STEPR(I),SMAX)
+*          
+*       Reduce irregular step if STEPR < STEP.
+ 25       IF (STEPR(I).LT.STEP(I)) THEN
+             STEP(I) = 0.5D0*STEP(I)
+             GO TO 25
+          END IF
+*
+*       Initialize or update array for new block times.
+          TIMENW(I) = T0(I) + STEP(I)
+*
+*       Set prediction variables (X0DOT set by START, KSREG or KSTERM).
+          DO 30 K = 1,3
+              X0(K,I) = X(K,I)
+              F(K,I) = 0.5D0*F(K,I)
+              FDOT(K,I) = ONE6*FDOT(K,I)
+   30     CONTINUE
+   40 CONTINUE
+!$omp end parallel do
+      
+*
+      RETURN
+*
+      END
---
+++ src/Main/tstep2.f
@@ -0,0 +1,22 @@
+      REAL*8 FUNCTION TSTEP2(XDOT,F,FDOT,ETA)
+*
+*
+*       General time-step criterion by xdot, f and fdot.
+*       ----------------------------
+*
+      IMPLICIT NONE
+      REAL*8  XDOT(3),F(3),FDOT(3),F2DOT(3),F3DOT(3)
+      REAL*8 F2,FDOT2,V2,ETA
+*
+*       Obtain new integration step using composite expression.
+*       STEP = (ETA*(F*F2DOT + FDOT**2)/(FDOT*F3DOT + F2DOT**2))**0.5.
+*
+      V2 = XDOT(1)**2 + XDOT(2)**2 + XDOT(3)**2
+      F2 = F(1)**2 + F(2)**2 + F(3)**2
+      FDOT2 = FDOT(1)**2 + FDOT(2)**2 + FDOT(3)**2
+*
+      TSTEP2 = 0.25*ETA*SQRT(MIN(V2/F2,F2/FDOT2))
+*
+      RETURN
+*
+      END

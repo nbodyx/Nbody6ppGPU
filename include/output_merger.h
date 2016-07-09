46e756619835cf115c2673d86b974c0d724553f7
Long Wang <longwang.astro@gmail.com>
2016-07-09 12:24:33 +0800

    Add the global output
    The new global output is status.36
    The detailed output data is written in comments in global_params_gether.f and global_output.F
    The nb6.py and nb6_status.ipynb are used to read the data.
    nb6.py include a function to read the data and nb6_status.ipynb is the Jupyter notebook to show how to use this function and how to draw the data.
    
    The current version is for testing. Many output data need to be check.

---
+++ include/output_merger.h
@@ -0,0 +1,31 @@
+*     Merger
+      COMMON/OUTMERGER/ M_M1(MMAX), M_M2(MMAX), M_M3(MMAX),
+     &                  M_XC1(MMAX), M_XC2(MMAX), M_XC3(MMAX),
+     &                  M_VC1(MMAX), M_VC2(MMAX), M_VC3(MMAX),
+     &                  M_XR01(MMAX), M_XR02(MMAX), M_XR03(MMAX), 
+     &                  M_VR01(MMAX), M_VR02(MMAX), M_VR03(MMAX), 
+     &                  M_XR11(MMAX), M_XR12(MMAX), M_XR13(MMAX), 
+     &                  M_VR11(MMAX), M_VR12(MMAX), M_VR13(MMAX), 
+     &                  M_FC1(MMAX), M_FC2(MMAX), M_FC3(MMAX),
+*     &                  M_FDC1(MMAX), M_FDC2(MMAX), M_FDC3(MMAX),
+     &                  M_RS1(MMAX), M_L1(MMAX), M_TE1(MMAX), 
+     &                  M_RS2(MMAX), M_L2(MMAX), M_TE2(MMAX), 
+     &                  M_RS3(MMAX), M_L3(MMAX), M_TE3(MMAX), 
+     &                  M_RC1(MMAX), M_MC1(MMAX), M_RC2(MMAX), 
+     &                  M_MC2(MMAX), M_RC3(MMAX), M_MC3(MMAX),
+     &                  M_A0(MMAX), M_ECC0(MMAX), M_P0(MMAX),
+     &                  M_A1(MMAX), M_ECC1(MMAX), M_P1(MMAX),
+     &                  M_POT(MMAX),
+     &                  NM_KW1(MMAX), NM_NAM1(MMAX), NM_KW2(MMAX),
+     &                  NM_NAM2(MMAX), NM_KW3(MMAX), NM_NAM3(MMAX),
+     &                  NM_KWC(MMAX), NM_NAMC(MMAX)
+      REAL*4 M_M1, M_M2, M_M3, M_XC1, M_XC2, M_XC3, M_VC1, M_VC2, M_VC3
+      REAL*4 M_XR01, M_XR02, M_XR03, M_VR01, M_VR02, M_VR03
+      REAL*4 M_XR11, M_XR12, M_XR13, M_VR11, M_VR12, M_VR13
+      REAL*4 M_FC1, M_FC2, M_FC3
+*      REAL*4 M_FDC1, M_FDC2, M_FDC3
+      REAL*4 M_RS1, M_L1, M_TE1, M_RS2, M_L2, M_TE2, M_RS3, M_L3, M_TE3
+      REAL*4 M_RC1, M_MC1, M_RC2, M_MC2, M_RC3, M_MC3
+      REAL*4 M_A0, M_ECC0, M_P0, M_A1, M_ECC1, M_P1, M_POT
+      INTEGER NM_KW1, NM_NAM1, NM_KW2, NM_NAM2
+      INTEGER NM_KW3, NM_NAM3, NM_KWC, NM_NAMC

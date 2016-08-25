a85b1783fc0f042ec6f74ab2bc38dba3790ee2d5
Long Wang <longwang.astro@gmail.com>
2016-08-25 16:33:11 +0900

    Set a binary criterion for KS binaries in data output
    Before all KS pairs are treated as binaries in output data.
    But actually a part of KS pairs may not be binaries (e.g. hyperbolic encoutners or some very wide pairs with many low mass stars between them)
    Thus now a binary criterion (Gamma < 0.1 and binding energy <0) is given to avoid include this binaries.
    Notice the Non-KS binaries can still have Gamma > 0.1.
    
    Now the Gamma is added in the output for binaries.

--- include/output_binary.h
+++ include/output_binary.h
@@ -10,11 +10,12 @@
      &                  B_RS2(KMAX), B_L2(KMAX), B_TE2(KMAX),
      &                  B_RC1(KMAX), B_MC1(KMAX), B_RC2(KMAX), 
      &                  B_MC2(KMAX), B_A(KMAX), B_ECC(KMAX), B_P(KMAX),
+     &                  B_G(KMAX),
      &                  NB_KW1(KMAX), NB_NAM1(KMAX), NB_KW2(KMAX), 
      &                  NB_NAM2(KMAX), NB_KWC(KMAX), NB_NAMC(KMAX)
       REAL*4 B_M1, B_M2, B_XC1, B_XC2, B_XC3, B_VC1, B_VC2, B_VC3
       REAL*4 B_XR1, B_XR2, B_XR3, B_VR1, B_VR2, B_VR3
-      REAL*4 B_FC1, B_FC2, B_FC3
+      REAL*4 B_FC1, B_FC2, B_FC3, B_G
 *      REAL*4 B_FDC1, B_FDC2, B_FDC3
       REAL*4 B_RS1, B_L1, B_TE1, B_RS2, B_L2, B_TE2
       REAL*4 B_RC1, B_MC1, B_RC2, B_MC2,B_A, B_ECC, B_P, B_POT

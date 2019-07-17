2e04014d37aeb352fed6dc31959e1f442cda0a37
Long Wang <longwang.astro@gmail.com>
2019-07-17 18:28:17 +0200

    Implement MWpontetial14

--- include/MWpotential.h
+++ include/MWpotential.h
@@ -9,11 +9,13 @@
 *     Tidal force for Milky-Way potential
       COMMON/MWPOT/ B_AMP, B_ALPHA, B_RC, B_NORM,
      &     D_AMP, D_A, D_B, D_NORM,
-     &     H_AMP, H_A, H_NORM, F_SCALE
+     &     H_AMP, H_A, H_NORM, 
+     &     F_PcMyr2, FD_PcMyr3, V_PcMyr, R_KPC, P_kpcPcMyr2
 
       REAL*8 B_AMP, B_ALPHA, B_RC, B_NORM
       REAL*8 D_AMP, D_A, D_B, D_NORM  
-      REAL*8 H_AMP, H_A, H_NORM, F_SCALE
+      REAL*8 H_AMP, H_A, H_NORM
+      REAL*8 F_PcMyr2, FD_PcMyr3, V_PcMyr, R_KPC, P_kpcPcMyr2
       
       
       

2cdb009878ecfa1b00d360e7921d89e2766900f6
Long Wang <longwang.astro@gmail.com>
2019-08-12 17:18:17 +0200

    Fix the energy calculation of mwpot

--- include/galaxy.h
+++ include/galaxy.h
@@ -1,5 +1,6 @@
 *     Tidal force galaxy parameters
       COMMON/GALAXY/ GMG,RG(3),VG(3),FG(3),FGD(3),TG,TGNEXT,
-     &               OMEGA,DISK,A,B,V02,RL2,GMB,AR,GAM,ZDUM(6)
+     &               OMEGA,DISK,A,B,V02,RL2,GMB,AR,GAM,EGPOT,EGKIN,
+     &               ZDUM(4)
       REAL*8 GMG,RG,VG,FG,FGD,TG,OMEGA,DISK,A,B,V02,RL2,GMB,AR,GAM,ZDUM
-      REAL*8 TGNEXT
+      REAL*8 TGNEXT,EGPOT,EGKIN
